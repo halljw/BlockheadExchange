4f5e1151471703850cc03de517ffdb61707b6f8c
bheath015 <31784768+bheath015@users.noreply.github.com>
2018-02-08 18:02:29 -0500

    added chart code, updated filepath for BC image in custom css

---
+++ Code/landing_chart.py
@@ -0,0 +1,111 @@
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
+import datetime as dt
+import matplotlib.ticker as ticker
+
+class FileWork(object):
+	"""docstring for FileWork"""
+	def __init__(self):
+		x = 12
+
+	def find_largest_swing(self):
+		currency_list = ['Bitcoin', 'Litecoin', 'Monero', 'Bitcoin-Cash', 'Bitcoin-Gold',
+		'Cardano', 'Dash', 'EOS', 'Ethereum-Classic', 'Ethereum', 'IOTA', 'NEM', 'NEO',
+		'Qtum', 'Ripple', 'Stellar-Lumens']
+		currency_dict = {}
+		for currency in currency_list:
+			currency_file = '../Data/' + currency + '.txt'
+			print (currency_file)
+			with open(currency_file, 'rb') as coin_file:
+				coin_array = np.genfromtxt(coin_file, delimiter=',', dtype=None)
+				compare_array_string = coin_array[-7::6]
+				compare_array = compare_array_string.astype(np.float)
+				vol_diff = (compare_array[0][5] - compare_array[1][5]) / ((compare_array[0][5] + compare_array[1][5]) / 2)
+				currency_dict[currency_file]=abs(vol_diff)
+				print (vol_diff)
+		print (currency_dict)
+		largest_vol_swing = '../Data/Bitcoin.txt'
+		for currency in currency_dict:
+			if currency_dict[currency] >= currency_dict[largest_vol_swing]:
+				largest_vol_swing = currency
+		print (largest_vol_swing)
+		largest_vol_swing_str = largest_vol_swing[8:-4]
+		self.create_chart(largest_vol_swing, largest_vol_swing_str, '../Data/Bitcoin.txt')
+
+	def txt_to_csv(self, input_file):
+		file = open(input_file, "r")
+
+		String = ""
+		for line in file:
+		    String += line
+
+		file.close()
+
+		file = open('volume.csv', 'w')
+		output_string = String[-8100::]
+		file.write(output_string)
+		file.close()
+
+	def create_chart(self, input_file, coin_name, btc_file):
+		#dataset1 setup
+		compare_data = pd.read_csv(input_file)
+		btc_data = pd.read_csv(btc_file)
+		compare_data.rename(columns={'VOL' : 'VOLUME'}, inplace=True)
+		compare_data['15 DAY ROLLING MEAN'] = pd.rolling_mean(compare_data['CLOSE'],15)
+		top = compare_data.head(0)
+		bottom = compare_data.tail(90)
+		recent_quarter = pd.concat([top, bottom])
+		recent_quarter['DATE'] = pd.to_datetime(recent_quarter['DATE'], format='%Y%m%d')
+		recent_quarter.set_index('DATE', inplace=True)
+		print (compare_data.tail())
+
+		#chart1 manipulation
+		chart = recent_quarter[['CLOSE', '15 DAY ROLLING MEAN']].plot()
+		chart.grid(color='k', axis='y', linestyle='dotted')
+		chart.set_xlabel('Date', fontname='Trebuchet MS', fontsize=16)
+		chart.set_ylabel('Price', fontname='Trebuchet MS', fontsize=16)
+		chart.legend(fancybox=True)
+		chart.set_facecolor('#90f1a3')
+		chart.set_title("%s 90 Day Price Chart" % coin_name, fontname='Trebuchet MS', fontsize=20)
+		
+		#dataset2 setup
+		fig = plt.figure()
+		ax1 = plt.subplot2grid((2,1), (0,0))
+		ax2 = plt.subplot2grid((2,1), (1,0), sharex=ax1)
+		v_top = compare_data.head(0)
+		v_bottom = compare_data.tail(14)
+		fourteen = pd.concat([v_top, v_bottom])
+
+		#dataset2a setup
+		fourteen_top = fourteen.reset_index()
+		fourteen_top = fourteen_top.drop('index', 1)
+		fourteen_top['DATE'] = pd.to_datetime(fourteen_top['DATE'], format='%Y%m%d')
+		fourteen_top['DATE'] = fourteen_top['DATE'].map(mdates.date2num)
+
+		#chart2a setup
+		candlestick_ohlc(ax1, fourteen_top.values, colorup='#53c156', colordown='#ff1717', width=.7)
+		ax1.set_title("%s 14 Day Chart" % coin_name, fontname='Trebuchet MS', fontsize=20)
+		ax1.set_ylabel('Price', fontname='Trebuchet MS', fontsize=16)
+
+		fourteen_bottom = fourteen
+		fourteen_bottom['DATE'] = pd.to_datetime(fourteen_bottom['DATE'], format='%Y%m%d')
+		fourteen_bottom['DATE'] = fourteen_bottom['DATE'].map(mdates.date2num)
+		fourteen_bottom.set_index('DATE', inplace=True)
+		print (fourteen_bottom.head())
+		print(fourteen_bottom)
+		fourteen_bottom['VOLUME'] = fourteen_bottom['VOLUME'] / 1000000
+		ax2.bar(fourteen_bottom[['VOLUME']].index, fourteen_bottom['VOLUME'])
+		ax2.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+		ax2.xaxis_date()
+		ax2.xaxis.set_major_formatter(mdates.DateFormatter("%m-%d"))
+		ax2.set_ylabel('Volume', fontname='Trebuchet MS', fontsize=16)
+
+		plt.show()
+
+if __name__=='__main__':
+	fw = FileWork()
+	fw.find_largest_swing()
